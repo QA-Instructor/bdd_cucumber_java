313467856122af8e4878741bb6258dd20580ed9e
Victoria Lloyd <QAInstructorSky@gmail.com>
2024-08-15 14:12:30 +0100

    End of Additio Scenario

---
+++ src/test/java/CalculatorStepDefinitions.java
@@ -0,0 +1,32 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorStepDefinitions {
+
+    private Calculator calc;
+
+    @Given("a calculator I just turned on")
+    public void a_calculator_i_just_turned_on() {
+        calc = new Calculator();
+    }
+    @When("I add {int} and {int}")
+    public void i_add_and(Integer int1, Integer int2) {
+        calc.push(int1);
+        calc.push(int2);
+        calc.push("+");
+    }
+    @Then("the result is {double}")
+    public void the_result_is(double expected) {
+        Number value = calc.value();
+        assertEquals(expected, value);
+    }
+
+    @When("I subtract {int} from {int}")
+    public void i_subtract_from(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
